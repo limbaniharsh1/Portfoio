e2bfa362103660bb2b99b0bd939630e1b4896c9f
harshlimbani <limbaniharsh1@gmail.com>
2023-07-10 10:42:25 +0530

    typewriter-effect

--- src/Components/Person.jsx
+++ src/Components/Person.jsx
@@ -1,26 +1,44 @@
 import React from "react";
 import { Download, Github, Linkedin, Telephone } from "react-bootstrap-icons";
+import TypewriterComponent from "typewriter-effect";
 
 function Person() {
   return (
-    <div className="person" style={{zIndex:99}}>
+    <div className="person" style={{ zIndex: 99 }}>
       <div className="person-img">
         <div className="img">
-          <img src="https://media.licdn.com/dms/image/D4E03AQEOGh_9COab6g/profile-displayphoto-shrink_800_800/0/1640755144164?e=2147483647&v=beta&t=V_CEcS5NhukqXctU5e_OBCl4W8ZeX1cj1nDSGsc4WPs" alt="img"/>
+          <img
+            src="https://media.licdn.com/dms/image/D4E03AQEOGh_9COab6g/profile-displayphoto-shrink_800_800/0/1640755144164?e=2147483647&v=beta&t=V_CEcS5NhukqXctU5e_OBCl4W8ZeX1cj1nDSGsc4WPs"
+            alt="img"
+          />
         </div>
       </div>
       <div className="person-details">
         <div>
           <h2>Harsh limbani</h2>
-          <p>front end developer</p>
+          {/* <p>front end developer</p> */}
+          <p>
+            <TypewriterComponent
+              options={{
+                autoStart: true,
+                delay: 40,
+                loop: true,
+                strings: ["front end developer", "web designer"],
+              }}
+            />
+          </p>
           <div>
             <Github className="icon" />
             <Linkedin className="icon" />
           </div>
         </div>
         <div className="bottom-person">
-          <div className="download">download_cv <Download className="mx-3"/></div>
-          <div className="contect">contect_me <Telephone className="mx-3"/></div>
+          <div className="download">
+            download_cv <Download className="mx-3" />
+          </div>
+          <div className="contect">
+            contect_me <Telephone className="mx-3" />
+          </div>
         </div>
       </div>
     </div>
