e50a734372e08cde3946784daa787b5165ce5107
harsh limbani <limbaniharsh1@gmail.com>
2023-09-01 15:03:58 +0530

    n

--- src/Components/Person.jsx
+++ src/Components/Person.jsx
@@ -10,7 +10,7 @@ function Person() {
       <div className="person-img">
         <div className="img">
           <img
-            src="https://media.licdn.com/dms/image/D4E03AQEOGh_9COab6g/profile-displayphoto-shrink_800_800/0/1640755144164?e=2147483647&v=beta&t=V_CEcS5NhukqXctU5e_OBCl4W8ZeX1cj1nDSGsc4WPs"
+            src=""
             alt="img"
           />
         </div>
